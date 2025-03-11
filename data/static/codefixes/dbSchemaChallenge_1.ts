1716f64211092f4597d79f3e897bdb20776a0be1
VeracodeVince <84136833+VeracodeVince@users.noreply.github.com>
2025-03-11 15:14:57 -0500

    Update dbSchemaChallenge_1.ts

--- data/static/codefixes/dbSchemaChallenge_1.ts
+++ data/static/codefixes/dbSchemaChallenge_1.ts
@@ -1,16 +1,27 @@
+// Custom sanitizer function to escape special SQL wildcard chars
+function sanitizeCriteria(input) {
+  return input.replace(/[%_'"]/g, '');
+}
+
 module.exports = function searchProducts () {
-  return (req: Request, res: Response, next: NextFunction) => {
-    let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
+  return (req, res, next) => {
+    let criteria = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query("SELECT * FROM Products WHERE ((name LIKE '%"+criteria+"%' OR description LIKE '%"+criteria+"%') AND deletedAt IS NULL) ORDER BY name")
-      .then(([products]: any) => {
+
+    // Applying custom sanitizer explicitly
+    const safeCriteria = sanitizeCriteria(criteria);
+
+    // ok: express-sequelize-injection
+    models.sequelize.query(
+      "SELECT * FROM Products WHERE ((name LIKE '%" + safeCriteria + "%' OR description LIKE '%" + safeCriteria + "%') AND deletedAt IS NULL) ORDER BY name"
+    ).then(([products]) => {
         const dataString = JSON.stringify(products)
         for (let i = 0; i < products.length; i++) {
           products[i].name = req.__(products[i].name)
           products[i].description = req.__(products[i].description)
         }
         res.json(utils.queryResultToJson(products))
-      }).catch((error: ErrorWithParent) => {
+      }).catch((error) => {
         next(error.parent)
       })
   }
